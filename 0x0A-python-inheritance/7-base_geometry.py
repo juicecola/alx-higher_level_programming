bcea2261e5e3cdc90916a298cbc3aca2b320fc0e
Eric Munyi <irerieric@gmail.com>
2023-01-09 18:47:03 -0800

    class BaseGeometry (based on 6-base_geometry.py).

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,3 +8,11 @@ class BaseGeometry:
     def area(self):
         """method not implemented yet"""
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates a value as an integer
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
