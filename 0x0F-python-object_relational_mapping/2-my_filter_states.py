76bacbf978b0e3be4a45047fc6261c893be21290
Eric Munyi <irerieric@gmail.com>
2023-03-19 15:00:16 -0700

    cript that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" 2-my_filter_states.py
+    takes in an argument and displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import sys
+import MySQLdb
+
+
+def main():
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    search = sys.argv[4]
+
+    db = MySQLdb.connect(
+        "localhost",
+        mysql_username,
+        mysql_password,
+        database_name,
+        3306)
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(search))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
+
